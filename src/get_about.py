c2ee22cff7d81085a76061076cbc1e6af82b7a55
robot <robot@guofei.site>
2022-08-28 17:27:02 +0800

    update

--- src/get_about.py
+++ src/get_about.py
@@ -2,7 +2,14 @@ import requests
 
 about_url = 'https://raw.githubusercontent.com/guofei9987/guofei9987.github.io/master/pages/about.md'
 
-resp = requests.get(about_url)
+r = requests.get(about_url)
 
-with open('about.md', 'wb') as f:
-    f.write(resp.content)
+content = r.content.decode('utf-8').replace('''---
+layout: page
+title: 关于
+---
+
+''', '')
+
+with open('README.md', 'w') as f:
+    f.write(content)
