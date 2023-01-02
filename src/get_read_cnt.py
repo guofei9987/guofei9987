bde017f95fd542cbd82e3f5225ae0e996fd8f9de
robot <robot@guofei.site>
2023-01-02 21:22:27 +0800

    新增json文件，统一管理数据

--- src/get_read_cnt.py
+++ src/get_read_cnt.py
@@ -1,4 +1,5 @@
 import json
+from bs4 import BeautifulSoup
 
 with open('achievement.json', 'r') as f:
     achievement = json.load(f)
@@ -15,12 +16,18 @@ achievement['book_cnt'] = len(book_list)
 # 在线：不容易很好的获取"读书库"的文本内容
 # 因此在另一个库里面计算字数，这里直接引用进来
 
-from bs4 import BeautifulSoup
 
 r = requests.get('https://www.guofei.site/reading/media/reading_words.svg')
 
 soup = BeautifulSoup(r.text, 'lxml')
 achievement['reading_words'] = soup.svg.text.strip()
+
+#%% 获取博客数量
+
+r = requests.get('https://www.guofei.site/pages/blog_cnt.svg')
+
+soup = BeautifulSoup(r.text, 'lxml')
+achievement['blog_cnt'] = soup.svg.text.strip()
 
 # %% 写入
 with open('achievement.json', 'w') as f:
