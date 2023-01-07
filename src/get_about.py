05b59c951d3d0bd59f31b8e0b75ff25031d493fa
robot <robot@guofei.site>
2023-01-07 21:46:25 +0800

    从网页端获取readme，并复制到本地

--- src/get_about.py
+++ src/get_about.py
@@ -1,45 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
-import json
-
-with open('achievement.json', 'r') as f:
-    achievement = json.load(f)
-
-about_url = 'https://raw.githubusercontent.com/guofei9987/guofei9987.github.io/master/pages/about.md'
+about_url = 'https://www.guofei.site/pages/about.html'
 
 r = requests.get(about_url)
+soup = BeautifulSoup(r.text, 'lxml')
 
-content = r.content.decode('utf-8') \
-    .replace('''---
-layout: page
-title: 关于
----
-
-''', '') \
-    .replace('{{ site.posts.size }}', str(achievement['cnt_blog'])) \
-    .replace('{{ site.data.cnt_reading_words.cnt_reading_words }}', str(achievement['cnt_reading_words'])). \
-    replace('{{ site.data.book_list.size }}', str(achievement['cnt_book'])) \
-    .replace('''我的项目：
-
-<table>
-<tr>
-  <th>Project</th>
-  <th>Star</th>
-  <th>Fork</th>
-  <th>description</th>
-</tr>
-
-{% assign sorted_repos = (site.github.public_repositories | sort: 'stargazers_count') | reverse | where: "fork", "false" %}
-{% for repo in sorted_repos | limit: site.side_bar_repo_limit %}
-<tr>
-  <td><a href="{{ repo.html_url }}">{{ repo.name }}</a></td>
-  <td>☆{{ repo.stargazers_count }}</td>
-  <td><img alt="fork:" src="https://www.guofei.site/public/icon/fork.svg">{{ repo.forks_count }}</td>
-  <td>{{ repo.description | truncate:30 }}</td>
-</tr>
-{% endfor %}
-</table>''', '')
-# 这个暂时先这样
-
-with open('README.md', 'w') as f:
-    f.write(content)
+about = soup.find(name='div', attrs={"id": "page-content"})
+with open('README2.md', 'w') as f:
+    f.write(about.decode_contents())
