56760b1e599664f95ab4d1986b59128cf3b5e331
郭飞 <guofei9987@foxmail.com>
2023-05-17 09:25:50 +0800

    Update get_about.py

--- src/get_about.py
+++ src/get_about.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-about_url = 'https://www.guofei.site/pages/about.html'
+about_url = 'https://www.guofei.site/about.html'
 
 r = requests.get(about_url)
 soup = BeautifulSoup(r.text, 'lxml')
