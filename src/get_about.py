37abdd200c537e10b4bb266252b199bb433c27f1
robot <robot@guofei.site>
2025-02-10 11:47:58 +0800

    m

--- src/get_about.py
+++ src/get_about.py
@@ -10,7 +10,7 @@ about = soup.find(name='div', attrs={"id": "page-content"})
 # img 链接无法生效。但 object 在 github 首页不显示
 with open('README.md', 'w') as f:
     f.write(about.decode_contents().
-            replace('''<object data="https://www.guofei.site/pages/trophy.svg"></object>''',
+            replace('''<object data="https://www.guofei.site/pages/trophy.svg" style="width: 100%;max-width: 550px;"></object>''',
                     '''<img src="https://www.guofei.site/pages/trophy.svg">''')
             .replace(
         '''<span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa  fa-stack-1x fa-inverse">知</i></span>''',
