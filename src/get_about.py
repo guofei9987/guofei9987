78547b9aa0477093e83e83182349776b6a8f3939
guofei9987 <guofei9987@foxmail.com>
2025-06-28 14:00:00 +0800

    update

--- src/get_about.py
+++ src/get_about.py
@@ -10,6 +10,6 @@ about = soup.find(name='div', attrs={"id": "page-content"})
 # img 链接无法生效。但 object 在 github 首页不显示
 with open('README.md', 'w') as f:
     f.write(about.decode_contents().
-            replace('''<object data="https://www.guofei.site/pages/trophy.svg" style="width: 100%;max-width: 550px;"></object>''',
-                    '''<img src="https://www.guofei.site/pages/trophy.svg">''')
+            replace('''<object data="https://www.guofei.site/trophy.svg" style="width: 100%;max-width: 550px;"></object>''',
+                    '''<img src="https://www.guofei.site/trophy.svg">''')
             )
